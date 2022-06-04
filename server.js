ffa35564d717e15c037b7aea3b5fdf949b7bc0a8
Phil <moreaerodynamic@gmail.com>
2022-06-04 12:04:10 -0500

    working rapper name  api

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancellor',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'soulja slim': {
+        'age': 26,
+        'birthName': 'James Adarryl Tapp Jr',
+        'birthLocation': 'New Orleans, Louisiana'
+    },
+    'unknown': {
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
+}
+
+
+// NETWORK REQUEST
+app.get('/', (request, response) => {
+    // tell network where to find file, 
+    response.sendFile(__dirname + '/index.html')
+})
+
+// API GET REQUEST, RESPOND WITH JSON
+app.get('/api/:name', (request, response) => {
+    const rapperName = request.params.name.toLowerCase()
+    // if rapperName is a property of wrappers
+    if(rappers[rapperName]) {
+        response.json(rappers[rapperName])
+    } else {
+        response.json(rappers['unknown'])
+    }
+})
+
+// NETWORK PORT
+app.listen(PORT, ()=> {
+    console.log(`THe server is now running on ${PORT}! Better go catch it!`)
+})
